be2144658c7f82d1cdea7af7b466f776d1203b84
learnaholic-bit <george.m.wessa+github@gmail.com>
2024-09-30 23:37:31 +0300

    draft menu selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { NavtopComponent } from "./nav/navtop/navtop.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, RouterModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
